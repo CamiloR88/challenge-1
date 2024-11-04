024fc284d69ee05572ca2e665cdc10c15862d756
CamiloR88 <camilordriguez88@gmail.com>
2024-11-03 21:28:52 -0600

    1ro

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{"Young-Serif":["Young Serif","sans-serif"]}
+    },
+  },
+  plugins: [],
+}
+
